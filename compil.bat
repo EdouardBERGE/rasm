1c2fb98935dabc653ad02593bc62315a3b9a8540
Edouard BERGE <edouard.berge@gmail.com>
2021-02-02 23:01:04 +0100

    ZX0 links (need to be tested soon)

--- compil.bat
+++ compil.bat
@@ -1,3 +1,7 @@
+cl ZX0-main\src\compress.c      /O2 /Qpar /Ob3 /c -I ZX0-main\src
+cl ZX0-main\src\optimize.c      /O2 /Qpar /Ob3 /c -I ZX0-main\src
+cl ZX0-main\src\memory.ci       /O2 /Qpar /Ob3 /c -I ZX0-main\src
+
 cl apultra-master\src\expand.c      /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include -I apultra-master\src
 cl apultra-master\src\matchfinder.c /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include -I apultra-master\src
 cl apultra-master\src\shrink.c      /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include -I apultra-master\src
@@ -19,7 +23,7 @@ cl lzsa-master\src\libdivsufsort\lib\divsufsort_utils.c /O2 /Qpar /Ob3 /c -I lzs
 cl lzsa-master\src\libdivsufsort\lib\sssort.c           /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include
 cl lzsa-master\src\libdivsufsort\lib\trsort.c           /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include
 
-cl rasm.c /O2 expand.obj matchfinder.obj shrink.obj dictionary.obj expand_block_v1.obj expand_block_v2.obj expand_context.obj expand_inmem.obj frame.obj matchfinder_lzsa.obj shrink_block_v1.obj shrink_block_v2.obj shrink_context.obj shrink_inmem.obj divsufsort.obj divsufsort_utils.obj sssort.obj trsort.obj
+cl rasm.c /O2 expand.obj matchfinder.obj shrink.obj dictionary.obj expand_block_v1.obj expand_block_v2.obj expand_context.obj expand_inmem.obj frame.obj matchfinder_lzsa.obj shrink_block_v1.obj shrink_block_v2.obj shrink_context.obj shrink_inmem.obj divsufsort.obj divsufsort_utils.obj sssort.obj trsort.obj optimize.obj compress.obj memory.obj
 
 upx.exe --brute rasm.exe
 
