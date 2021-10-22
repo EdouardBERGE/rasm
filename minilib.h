485f1a550fdfae23f37cea546d9f832852929862
Edouard BERGE <edouard.berge@gmail.com>
2021-10-22 18:27:52 +0200

    stristr fonction

--- minilib.h
+++ minilib.h
@@ -110,6 +110,67 @@ size_t strnlen (s, maxlen)
 #define _strnlen strnlen
 #endif
 
+/*
+** Call syntax:  char *stristr(char *String, char *Pattern)
+**
+** Description:  This function is an ANSI version of strstr() with
+**               case insensitivity.
+** Return item:  char *pointer if Pattern is found in String, else
+**               pointer to 0
+** Rev History:  07/04/95  Bob Stout  ANSI-fy
+**               02/03/94  Fred Cole  Original
+** Hereby donated to public domain.
+** Modified for use with libcyrus by Ken Murchison 06/01/00.
+**
+** from Debian package cyrus-imapd
+*/
+
+#ifndef stristr
+char *stristr(const char *String, const char *Pattern)
+{
+      char *pptr, *sptr, *start;
+      uint  slen, plen;
+
+      for (start = (char *)String,
+           pptr  = (char *)Pattern,
+           slen  = strlen(String),
+           plen  = strlen(Pattern);
+
+           /* while string length not shorter than pattern length */
+
+           slen >= plen;
+
+           start++, slen--)
+      {
+            /* find start of pattern in string */
+            while (toupper(*start) != toupper(*Pattern))
+            {
+                  start++;
+                  slen--;
+
+                  /* if pattern longer than string */
+
+                  if (slen < plen)
+                        return(NULL);
+            }
+
+            sptr = start;
+            pptr = (char *)Pattern;
+
+            while (toupper(*sptr) == toupper(*pptr))
+            {
+                  sptr++;
+                  pptr++;
+
+                  /* if end of pattern then pattern was found */
+
+                  if ('\0' == *pptr)
+                        return (start);
+            }
+      }
+      return(NULL);
+}
+#endif
 
 char *TxtStrDupLen(char *str, int *len)
 {
