ef7dddcc15d07563c9aeabcace854fb0ab9a8a63
Edouard BERGE <edouard.berge@gmail.com>
2021-10-22 10:54:55 +0200

    new opts

--- decrunch/dzx0_standard_back.asm
+++ decrunch/dzx0_standard_back.asm
@@ -7,30 +7,31 @@
 ;   DE: last destination address (decompressing)
 ; -----------------------------------------------------------------------------
 
-dzx0_standard_back:
+Macro dzx0_standard_back
         ld      bc, 1                   ; preserve default offset 1
         push    bc
-        dec     c
         ld      a, $80
-dzx0sb_literals:
-        call    dzx0sb_elias            ; obtain length
+@dzx0sb_literals:
+        call    @dzx0sb_elias            ; obtain length
         lddr                            ; copy literals
+        inc     c
         add     a, a                    ; copy from last offset or new offset?
-        jr      c, dzx0sb_new_offset
-        call    dzx0sb_elias            ; obtain length
-dzx0sb_copy:
+        jr      c, @dzx0sb_new_offset
+        call    @dzx0sb_elias            ; obtain length
+@dzx0sb_copy:
         ex      (sp), hl                ; preserve source, restore offset
         push    hl                      ; preserve offset
         add     hl, de                  ; calculate destination - offset
         lddr                            ; copy from offset
+        inc     c
         pop     hl                      ; restore offset
         ex      (sp), hl                ; preserve offset, restore source
         add     a, a                    ; copy from literals or new offset?
-        jr      nc, dzx0sb_literals
-dzx0sb_new_offset:
+        jr      nc, @dzx0sb_literals
+@dzx0sb_new_offset:
         inc     sp                      ; discard last offset
         inc     sp
-        call    dzx0sb_elias            ; obtain offset MSB
+        call    @dzx0sb_elias            ; obtain offset MSB
         dec     b
         ret     z                       ; check end marker
         dec     c                       ; adjust for positive offset
@@ -42,22 +43,23 @@ dzx0sb_new_offset:
         inc     bc
         push    bc                      ; preserve new offset
         ld      bc, 1                   ; obtain length
-        call    c, dzx0sb_elias_backtrack
+        call    c, @dzx0sb_elias_backtrack
         inc     bc
-        jr      dzx0sb_copy
-dzx0sb_elias:
-        inc     c                       ; inverted interlaced Elias gamma coding
-dzx0sb_elias_loop:
-        add     a, a
-        jr      nz, dzx0sb_elias_skip
-        ld      a, (hl)                 ; load another group of 8 bits
-        dec     hl
-        rla
-dzx0sb_elias_skip:
-        ret     nc
-dzx0sb_elias_backtrack:
+        jr      @dzx0sb_copy
+@dzx0sb_elias_backtrack:
         add     a, a
         rl      c
         rl      b
-        jr      dzx0sb_elias_loop
+@dzx0sb_elias:
+        add     a, a                    ; inverted interlaced Elias gamma coding
+        jr      nz, @dzx0sb_elias_skip
+        ld      a, (hl)                 ; load another group of 8 bits
+        dec     hl
+        rla
+@dzx0sb_elias_skip:
+        jr      c, @dzx0sb_elias_backtrack
+        ret
+mend
 ; -----------------------------------------------------------------------------
+
+
