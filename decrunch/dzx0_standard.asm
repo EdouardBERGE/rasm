513e4892a11597a007fd591939238693a4aa1d75
Edouard BERGE <edouard.berge@gmail.com>
2021-10-22 18:41:30 +0200

    macro version

--- decrunch/dzx0_standard.asm
+++ decrunch/dzx0_standard.asm
@@ -7,18 +7,18 @@
 ;   DE: destination address (decompressing)
 ; -----------------------------------------------------------------------------
 
-dzx0_standard:
+macro dzx0_standard
         ld      bc, $ffff               ; preserve default offset 1
         push    bc
         inc     bc
         ld      a, $80
-dzx0s_literals:
-        call    dzx0s_elias             ; obtain length
+@dzx0s_literals:
+        call    @dzx0s_elias             ; obtain length
         ldir                            ; copy literals
         add     a, a                    ; copy from last offset or new offset?
-        jr      c, dzx0s_new_offset
-        call    dzx0s_elias             ; obtain length
-dzx0s_copy:
+        jr      c, @dzx0s_new_offset
+        call    @dzx0s_elias             ; obtain length
+@dzx0s_copy:
         ex      (sp), hl                ; preserve source, restore offset
         push    hl                      ; preserve offset
         add     hl, de                  ; calculate destination - offset
@@ -26,11 +26,11 @@ dzx0s_copy:
         pop     hl                      ; restore offset
         ex      (sp), hl                ; preserve offset, restore source
         add     a, a                    ; copy from literals or new offset?
-        jr      nc, dzx0s_literals
-dzx0s_new_offset:
+        jr      nc, @dzx0s_literals
+@dzx0s_new_offset:
         pop     bc                      ; discard last offset
         ld      c, $fe                  ; prepare negative offset
-        call    dzx0s_elias_loop        ; obtain offset MSB
+        call    @dzx0s_elias_loop        ; obtain offset MSB
         inc     c
         ret     z                       ; check end marker
         ld      b, c
@@ -40,24 +40,25 @@ dzx0s_new_offset:
         rr      c
         push    bc                      ; preserve new offset
         ld      bc, 1                   ; obtain length
-        call    nc, dzx0s_elias_backtrack
+        call    nc, @dzx0s_elias_backtrack
         inc     bc
-        jr      dzx0s_copy
-dzx0s_elias:
+        jr      @dzx0s_copy
+@dzx0s_elias:
         inc     c                       ; interlaced Elias gamma coding
-dzx0s_elias_loop:
+@dzx0s_elias_loop:
         add     a, a
-        jr      nz, dzx0s_elias_skip
+        jr      nz, @dzx0s_elias_skip
         ld      a, (hl)                 ; load another group of 8 bits
         inc     hl
         rla
-dzx0s_elias_skip:
+@dzx0s_elias_skip:
         ret     c
-dzx0s_elias_backtrack:
+@dzx0s_elias_backtrack:
         add     a, a
         rl      c
         rl      b
-        jr      dzx0s_elias_loop
+        jr      @dzx0s_elias_loop
+mend
 ; -----------------------------------------------------------------------------
 
 
