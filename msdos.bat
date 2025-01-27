8f5deab0d2c2e1c3eb9a9ab059a31556394ca3f3
roudoudou <edouard.berge@gmail.com>
2025-01-27 22:19:49 +0100

    msdos compilation

--- msdos.bat
+++ msdos.bat
@@ -1,4 +1,4 @@
-wcl386 rasm.c -6r -6s -fp6 -d0 -k4000000 -ox /bt=DOS /l=dos4g -DOS_WIN=1 -DNOAPULTRA=1
-upx.exe --brute rasm.exe
+wcl386 rasm.c -6r -6s -fp6 -d0 -k4000000 -ox /bt=DOS /l=dos4g -DOS_WIN=1 -DNOAPULTRA=1 -DLZ4_DEPRECATED=1 -DNO_3RD_PARTIES=1
+
 
 
