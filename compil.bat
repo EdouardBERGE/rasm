f9d69e8dcf423e3934a020518e58f8c1f1275a9f
roudoudou <edouard.berge@gmail.com>
2025-11-09 00:13:24 +0100

    windows x64 script (unchecked)

--- compil.bat
+++ compil.bat
@@ -1,6 +1,6 @@
-cl ZX0-main\src\compress.c      /O2 /Qpar /Ob3 /c -I ZX0-main\src
-cl ZX0-main\src\optimize.c      /O2 /Qpar /Ob3 /c -I ZX0-main\src
-cl ZX0-main\src\memory.c        /O2 /Qpar /Ob3 /c -I ZX0-main\src
+cl salvador\src\matchfinder.c       /O2 /Qpar /Ob3 /c -I salvador\src -I lzsa-master\src\libdivsufsort\include /Fomatch_salvador.obj
+cl salvador\src\expand.c            /O2 /Qpar /Ob3 /c -I salvador\src -I lzsa-master\src\libdivsufsort\include /Foexpand_salvador.obj
+cl salvador\src\shrink.c            /O2 /Qpar /Ob3 /c -I salvador\src -I lzsa-master\src\libdivsufsort\include /Foshrink_salvador.obj
 
 cl apultra-master\src\expand.c      /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include -I apultra-master\src
 cl apultra-master\src\matchfinder.c /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include -I apultra-master\src
@@ -23,6 +23,5 @@ cl lzsa-master\src\libdivsufsort\lib\divsufsort_utils.c /O2 /Qpar /Ob3 /c -I lzs
 cl lzsa-master\src\libdivsufsort\lib\sssort.c           /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include
 cl lzsa-master\src\libdivsufsort\lib\trsort.c           /O2 /Qpar /Ob3 /c -I lzsa-master\src\libdivsufsort\include
 
-cl rasm.c /O2 expand.obj matchfinder.obj shrink.obj dictionary.obj expand_block_v1.obj expand_block_v2.obj expand_context.obj expand_inmem.obj frame.obj matchfinder_lzsa.obj shrink_block_v1.obj shrink_block_v2.obj shrink_context.obj shrink_inmem.obj divsufsort.obj divsufsort_utils.obj sssort.obj trsort.obj optimize.obj compress.obj memory.obj
+cl rasm.c /O2 expand.obj matchfinder.obj shrink.obj dictionary.obj expand_block_v1.obj expand_block_v2.obj expand_context.obj expand_inmem.obj frame.obj matchfinder_lzsa.obj shrink_block_v1.obj shrink_block_v2.obj shrink_context.obj shrink_inmem.obj divsufsort.obj divsufsort_utils.obj sssort.obj trsort.obj match_salvador.obj expand_salvador.obj shrink_salvador.obj
 
-upx.exe --ultra-brute rasm.exe
