53b4c9a819103250e41e3a128e7c0c279daa2b62
roudoudou <edouard.berge@gmail.com>
2025-12-04 09:06:00 +0100

    profiler in another project

--- buildProfiler.bat
+++
@@ -1,4 +0,0 @@
-cl z80-master\z80.c -I z80-master /O2 /Ob3 /c /Foz80.obj
-cl emuZ80.c /O2 z80.obj -DOS_WIN
-
-
--- buildProfiler.sh
+++
@@ -1,4 +0,0 @@
-cc -c -O2 z80-master/z80.c -o z80-master/z80.o
-cc emuZ80.c z80-master/z80.o -o emuZ80.exe -lm
-strip emuZ80.exe
-
